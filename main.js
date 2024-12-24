00c0f4bab1b6e3a32cb03674e220eb5354d79fba
Techwithjack123 <105387397+Techwithjack123@users.noreply.github.com>
2024-12-24 12:24:00 +0000

    Changed Port

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path');
 const app = express()
-const port = 3000
+const port = 80
 
 app.get('/countdown', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/countdown.html'));
