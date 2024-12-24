87f23371c98f6ce23a2c59ced24017170e929fdf
Techwithjack123 <105387397+Techwithjack123@users.noreply.github.com>
2024-12-24 12:18:49 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+const port = 3000
+
+app.get('/countdown', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/countdown.html'));
+})
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
